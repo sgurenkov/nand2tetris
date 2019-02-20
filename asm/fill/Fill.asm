21dec5f40b20dc225ac15d8a9b854ba6588827f9
Stan Gurenkov <Stan.Gurenkov@docusign.com>
2019-02-20 10:40:39 -0800

    rename assembly folder

--- asm/fill/Fill.asm
+++ asm/fill/Fill.asm
@@ -11,4 +11,29 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// 8192 bytes starting at @SCREEN address
+
+// set counter to zero
+@i
+M=0
+
+(LOOP)
+// calculate the current screen word address in memory
+@SCREEN
+D=A
+@i
+D=D+M
+
+// set all bits of current screen address word to 1
+A=D
+M=-1
+
+// increment counter
+@i
+M=M+1
+D=M
+@8192
+D=A-D
+
+@LOOP
+D;JGT
